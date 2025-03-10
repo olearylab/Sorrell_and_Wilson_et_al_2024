92acd09ab361fa2c4f10c54a4f65d535cf2992f9
olearylab <45610348+olearylab@users.noreply.github.com>
2025-03-10 16:58:34 +0000

    Delete figure_6/plot_figure_6_supplement_3.m

--- figure_6/plot_figure_6_supplement_3.m
+++
@@ -1,7 +0,0 @@
-%% Plot figure 6 supplement 3
-
-va_corrs = importdata("va_dec_corrs_29122023_cut.mat");
-yaw_corrs = importdata("yaw_dec_corrs_subs_29122023_cut.mat");
-
-%
-[h_boots] = plot_weights_changes_corrs(va_corrs,yaw_corrs);
